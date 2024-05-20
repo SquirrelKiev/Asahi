34fc738c3d67b11f24930efe14970e3dc804bc03
SquirrelKiev <laidbackenon@outlook.com>
2024-05-20 03:05:39 +0100

    Fix really bad bug im very suprised this ever made it in

--- Asahi/Modules/Highlights/HighlightsModule.cs
+++ Asahi/Modules/Highlights/HighlightsModule.cs
@@ -60,7 +60,7 @@ public class HighlightsModule(DbService dbService, HighlightsTrackingService hts
     {
         return CommonConfig(name, async (context, cleanName, _) =>
         {
-            var board = await context.HighlightBoards.FirstOrDefaultAsync(x => x.Name == name);
+            var board = await context.HighlightBoards.FirstOrDefaultAsync(x => x.GuildId == Context.Guild.Id && x.Name == name);
             if (board == null)
             {
                 return new ConfigChangeResult(false, $"`{name}` does not exist already.");
@@ -1062,7 +1062,7 @@ public class HighlightsModule(DbService dbService, HighlightsTrackingService hts
             highlightBoards = highlightBoards.Include(x => x.SpoilerChannels);
         }
 
-        var board = await highlightBoards.FirstOrDefaultAsync(x => x.Name == name);
+        var board = await highlightBoards.FirstOrDefaultAsync(x => x.GuildId == Context.Guild.Id && x.Name == name);
 
         if (board == null)
         {
