fd01907e29405d98d1a482735a9545ced5258686
SquirrelKiev <laidbackenon@outlook.com>
2024-08-11 17:36:58 +0100

    Regex fixes

--- Asahi/Core/CompiledRegex.cs
+++ Asahi/Core/CompiledRegex.cs
@@ -5,13 +5,13 @@ namespace Asahi;
 public static partial class CompiledRegex
 {
     //[GeneratedRegex(@"https?:\/\/(?:www\.)?reddit\.com\/r\/([0-9A-Za-z_]+)(?:\/[a-z]*)?\.json")]
-    [GeneratedRegex(@"reddit:(?<type>.)(?:\/(?<subreddit>[0-9A-Za-z_]+))")]
+    [GeneratedRegex(@"^reddit:(?<type>.)(?:\/(?<subreddit>[0-9A-Za-z_]+))$")]
     public static partial Regex RedditFeedRegex();
 
     [GeneratedRegex(@"^[\w-]+$")]
     public static partial Regex IsValidId();
     
-    [GeneratedRegex(@"https:\/\/(?:canary)?.discord.com\/channels\/[0-9]*\/[0-9]*\/([0-9]*)")]
+    [GeneratedRegex(@"^https:\/\/(?:canary)?.discord.com\/channels\/[0-9]*\/[0-9]*\/([0-9]*)$")]
     public static partial Regex MessageLinkRegex();
     
     [GeneratedRegex(@"```cs\n([\s\S]+?)\n```")]
