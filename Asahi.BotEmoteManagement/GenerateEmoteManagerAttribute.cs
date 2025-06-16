bbe0b3a016f3cfc46859a75d71e8a0d813030d1a
enonibobble <laidbackenon@outlook.com>
2025-06-16 01:07:14 +0100

    Generated code attribute applies to model class too

--- Asahi.BotEmoteManagement/GenerateEmoteManagerAttribute.cs
+++ Asahi.BotEmoteManagement/GenerateEmoteManagerAttribute.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿namespace Asahi.BotEmoteManagement;
 
-namespace Asahi.BotEmoteManagement;
-
+#pragma warning disable CS9113 // "Parameter is unread." Parameter is used by source gen.
 // goes on the manager to source gen
 [AttributeUsage(AttributeTargets.Class)]
 public sealed class GenerateEmoteManagerAttribute(Type specificationType) : Attribute
 { }
+#pragma warning restore CS9113 // Parameter is unread.
