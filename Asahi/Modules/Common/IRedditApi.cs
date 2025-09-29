186329c42164f25c9dff43673e3ed6205575cc71
enonibobble <laidbackenon@outlook.com>
2025-09-29 13:19:59 +0100

    fix(feeds): explicit sort by new parameter on the reddit api request
    surely its not this easy

--- Asahi/Modules/Common/IRedditApi.cs
+++ Asahi/Modules/Common/IRedditApi.cs
@@ -8,9 +8,9 @@ public interface IRedditApi
     [Get($"/r/{{{nameof(subreddit)}}}/about.json")]
     Task<ApiResponse<SubredditChild>> GetSubredditInfo(string subreddit, CancellationToken cancellationToken = default);
     
-    [Get($"/r/{{{nameof(subreddit)}}}/new.json?limit=25")]
+    [Get($"/r/{{{nameof(subreddit)}}}/new.json?sort=new&limit=25")]
     Task<ApiResponse<SubredditPosts>> GetSubredditPosts(string subreddit, CancellationToken cancellationToken = default);
     
-    [Get($"/r/{{{nameof(subreddit)}}}/new.json?limit=25")]
+    [Get($"/r/{{{nameof(subreddit)}}}/new.json?sort=new&limit=25")]
     Task<ApiResponse<string>> GetSubredditPostsRaw(string subreddit, CancellationToken cancellationToken = default);
 }
